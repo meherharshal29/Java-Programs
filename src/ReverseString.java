5172300aadab6f7691dc4376765b0833f0a2b66e
Harshal Meher <meherharshal29@gmail.com>
2025-08-29 16:09:41 +0530

    Update ReverseString.java

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -1,1 +1,16 @@
-public class ReverseString {    public static void main(String[] args) {        String name ="Harsh";        StringBuilder reverse = new StringBuilder(name).reverse();        System.out.println(reverse);    }}
+public class ReverseString {
+    public static void main(String[] args) {
+        String name ="Harsh";
+        StringBuilder reverse = new StringBuilder(name).reverse();
+        System.out.println(reverse);
+
+        //OR
+
+        char[] arr = name.toCharArray();
+        String rev = "";
+        for (int i = arr.length -1; i >=0 ; i--) {
+            rev+=arr[i];
+        }
+        System.out.println(rev);
+    }
+}
